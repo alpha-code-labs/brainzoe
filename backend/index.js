74b08c45d160d367adee024bbb45b2cb2ab6f3cb
Ajay Singh <ajayaxes318@gmail.com>
2024-09-09 18:28:49 +0530

    basic change for auth route to function

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const errorHanlder = require('./src/middlewares/error.middleware.js');
+const connectToDb = require('./src/config/db.js');
+const authRouter = require('./src/routes/auth.routes.js');
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 
-app.get('/', (req, res)=> res.status(200).json({message: "listening.."}))
 
+app.get('/', (req, res)=> {
+    throw new Error('Some Error occured', 'invaid parms');
+})
+
+app.use('/auth', authRouter);
+
+app.use(errorHanlder);
+
+connectToDb();
 app.listen(process.env.PORT, ()=>console.log(`server listening on port ${process.env.PORT}`))
