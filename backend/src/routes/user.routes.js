7892e3a6ca856e1d180cabf8dc578b66bd82ef84
Ajay Singh <ajayaxes318@gmail.com>
2024-09-11 16:18:03 +0530

    added route for getting user coins

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const {getUserProfile, updateUserCoins} = require('../controllers/user.controller.js');
+const {getUserProfile, updateUserCoins, getUserCoins} = require('../controllers/user.controller.js');
 const { authenticate } = require('../middlewares/authenticate.middleware.js');
 const router = express.Router();
 
 router.get('/', (req, res)=> res.send(200).json('This is user route'));
 router.get('/profile', authenticate, getUserProfile);
+router.get('/coins', authenticate, getUserCoins);
 router.patch('/coins', authenticate, updateUserCoins);
 
 
