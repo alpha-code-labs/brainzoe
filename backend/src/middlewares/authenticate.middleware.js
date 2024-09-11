e6ed9a63b13dd3a568a7602f3887f5e210b24d9c
Ajay Singh <ajayaxes318@gmail.com>
2024-09-11 16:15:05 +0530

    made more roubust: checking valid jwt format

--- backend/src/middlewares/authenticate.middleware.js
+++ backend/src/middlewares/authenticate.middleware.js
@@ -9,6 +9,7 @@ const authenticate = async (req, res, next)=>{
         console.log('token..', token)
 
         if(!token) throw new AuthenticationError('Token not provided');
+        if(!isJwt(token)) throw new AuthenticationError('Malformed jwt');
 
         const userId = jwtProvider.getUserIdFromToken(token);
         const user = await userService.findUserById(userId);
@@ -23,5 +24,21 @@ const authenticate = async (req, res, next)=>{
     }
 }
 
+
+const isJwt = (token) => {
+    if (!token) return false;
+  
+    const parts = token.split('.');
+    if (parts.length !== 3) return false;
+  
+    try {
+      // Validate Base64 or Base64URL encoding
+      const [header, payload] = parts;
+      const base64UrlPattern = /^[A-Za-z0-9-_]+$/; // Base64URL characters
+      return base64UrlPattern.test(header) && base64UrlPattern.test(payload);
+    } catch (e) {
+      return false;
+    }
+  };
 
 module.exports  = {authenticate};
