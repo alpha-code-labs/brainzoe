e962ba6ae2818e4c1e556e051086b22ff21f61e1
Ajay Singh <ajayaxes318@gmail.com>
2024-09-11 16:58:54 +0530

    minor changes

--- backend/src/middlewares/error.middleware.js
+++ backend/src/middlewares/error.middleware.js
@@ -3,21 +3,7 @@ const logError = (error)=>{
     return error;
 } 
 
-const createError = (type, message, statusCode) => ({
-    type,
-    message,
-    statusCode,
-});
 
-const handleError = (error) => {
-    if (error.name === 'ValidationError') {
-        return createError('ValidationError', error.message, 400);
-    } else if (error.code === 11000) {
-        return createError('DuplicateKeyError', 'Duplicate key error', 409);
-    } else {
-        return createError('UnknownError', error.message??error, 500);
-    }
-}
 
 const errorHanlder = (err, req, res, next) =>{
     console.log('middleware handling error.. ')
